c1b3f9fbc0e393ddde39fc485645cea6693b8eb5
Etienne Palmer-Campbell <palmercampbelle@gmail.com>
2019-03-16 20:37:13 -0500

    Initial Player/Ball shooter
    Brought over old code from another project for shooting balls and moving player

---
+++ Assets/Local Asset/Scripts/BallLauncher.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallLauncher : MonoBehaviour
+{
+    [SerializeField] private GameObject ballPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LaunchBall( float arcPower, float launchPower )
+    {
+        GameObject newBullet = GameObject.Instantiate(ballPrefab, transform.position, transform.rotation) as GameObject;
+        newBullet.GetComponent<Rigidbody>().velocity += Vector3.up * arcPower;
+        newBullet.GetComponent<Rigidbody>().velocity += transform.forward * launchPower;
+//         newBullet.GetComponent<Rigidbody>().AddForce( newBullet.transform.forward * launchPower );
+    }
+}
