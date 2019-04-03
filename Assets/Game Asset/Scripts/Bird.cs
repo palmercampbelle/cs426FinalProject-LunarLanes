d47033a53306870881625af94f50596dcfadf3ff
mhoward21 <48659275+mhoward21@users.noreply.github.com>
2019-04-03 03:10:39 -0500

    Revert "Revert "Added Bird AI""
    This reverts commit e33c6e2a1fe81fef02b8a52ce05251dc4de0fcb0.

---
+++ Assets/Game Asset/Scripts/Bird.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    GameObject cube;
+    public Transform center;
+    public Vector3 axis = Vector3.up;
+    public Vector3 desiredPosition;
+    public float radius = 5.0f;
+    public float radiusSpeed = 0.5f;
+    public float rotationSpeed = 80.0f;
+
+    int idleAnimationHash;
+    int singAnimationHash;
+    int ruffleAnimationHash;
+    int preenAnimationHash;
+    int peckAnimationHash;
+    int hopForwardAnimationHash;
+    int hopBackwardAnimationHash;
+    int hopLeftAnimationHash;
+    int hopRightAnimationHash;
+    int worriedAnimationHash;
+    int landingAnimationHash;
+    int flyAnimationHash;
+    int hopIntHash;
+    int flyingBoolHash;
+    //int perchedBoolHash;
+    int peckBoolHash;
+    int ruffleBoolHash;
+    int preenBoolHash;
+    //int worriedBoolHash;
+    int landingBoolHash;
+    int singTriggerHash;
+    int flyingDirectionHash;
+    int dieTriggerHash;
+
+    Animator anim;
+
+    void Start()
+    {
+        cube = GameObject.FindWithTag("orbit");
+        center = cube.transform;
+        transform.position = (transform.position - center.position).normalized * radius + center.position;
+        radius = 2.0f;
+
+        idleAnimationHash = Animator.StringToHash("Base Layer.Idle");
+        flyAnimationHash = Animator.StringToHash("Base Layer.fly");
+        hopIntHash = Animator.StringToHash("hop");
+        flyingBoolHash = Animator.StringToHash("flying");
+        peckBoolHash = Animator.StringToHash("peck");
+        ruffleBoolHash = Animator.StringToHash("ruffle");
+        preenBoolHash = Animator.StringToHash("preen");
+        landingBoolHash = Animator.StringToHash("landing");
+        singTriggerHash = Animator.StringToHash("sing");
+        flyingDirectionHash = Animator.StringToHash("flyingDirectionX");
+        dieTriggerHash = Animator.StringToHash("die");
+        anim = GetComponent<Animator>();
+        anim.SetFloat("IdleAgitated", 0.5f);
+        //anim.SetInteger(hopIntHash, -2);
+    }
+
+    void Update()
+    {
+        transform.RotateAround(center.position, axis, rotationSpeed * Time.deltaTime);
+        desiredPosition = (transform.position - center.position).normalized * radius + center.position;
+        transform.position = Vector3.MoveTowards(transform.position, desiredPosition, Time.deltaTime * radiusSpeed);
+    }
+}
